46454d6147f1a122051432a321c1d85b6894e8b6
--global <tmonssaf@gmail.com>
2018-09-22 16:48:33 -0400

    MT SM: calculateGlobalPCM

--- tp1/Plane.m
+++ tp1/Plane.m
@@ -4,29 +4,31 @@ classdef Plane
         massCenterPosition
     end
     methods
-        function obj = Plane(posA)
+        function obj = Plane()
             obj.parts = [
-                Cockpit(posA)
-                Body(posA)
-                Wing(posA, PartPosition.Left)
-                Wing(posA, PartPosition.Right)
-                Fin(posA)
-                Reactor(posA, PartPosition.Left)
-                Reactor(posA, PartPosition.Right)
+                Cockpit()
+                Body()
+                Wing(PartPosition.Left)
+                Wing(PartPosition.Right)
+                Fin()
+                Reactor(PartPosition.Left)
+                Reactor(PartPosition.Right)
             ];
             obj.massCenterPosition = calculateMassCenter();
         end
     end
+    
     methods(Static)
-        function  posA = calculateLocalPosA()
+        function  localPosA = calculateLocalPosA()
             z = BODY_RADIUS + WING_THICKNESS;
             y = 0;
             x = BODY_LENGTH + COCKPIT_LENGTH;
             
-            posA = [x,y,z];
+            localPosA = [x; y; z];
         end
     end
-    methods
+    
+    methods(Static)
         function massCenterPosition = calculateMassCenter()
             sumOfWeightedMassCenters = 0;
             for idx = 1:numel(obj.parts)
@@ -36,7 +38,16 @@ classdef Plane
             massCenterPosition = sumOfWeightedMassCenters / sum(vertcat(obj.parts.mass())); %arrayfun(@(part) part.getMass(), obj.parts)
         end
     end
-    methods
-        function 
+    
+    methods(Static)
+        function globalPCM = calculatePCMGlobalPosition(posA, ar)
+            localPosA = calculateLocalPosA();
+            localOrigin = [posA(1)- localPosA(1); posA(2)- localPosA(2); posA(3)- localPosA(3)];
+            centeredPCM = calculateMassCenter() + localOrigin - posA;
+            
+            rotationMatrix = [cos(ar), 0, sin(ar); 0, 1, 0; -sin(ar), 0, cos(ar)];
+            
+            globalPCM = rotationMatrix * centeredPCM + posA;
+        end
     end
 end
