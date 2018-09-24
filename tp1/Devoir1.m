03ca6a21e5206ae2095e2da87447070ecba26f33
William Harvey <wihar@gigl.polymtl.ca>
2018-09-24 13:21:14 -0400

    Adjusted main function

--- tp1/Devoir1.m
+++ tp1/Devoir1.m
@@ -1,3 +1,6 @@
 function [pcm, MI, aa] = Devoir1(posA, ar, va, Forces)
-    plane = Plane(posA);
+    plane = Plane();
+    pcm = plane.calculateGlobalPCM(posA, rad2deg(ar))
+    MI = [0; 0; 0]
+    aa = 0
 end
