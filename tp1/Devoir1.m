f199749c92de5bb87fb55fed367330a94319c7e7
William Harvey <william.harvey@polymtl.ca>
2018-09-25 15:47:15 -0400

    Added gravity force in torque equation

--- tp1/Devoir1.m
+++ tp1/Devoir1.m
@@ -1,6 +1,6 @@
 function [pcm, MI, aa] = Devoir1(posA, ar, va, Forces)
     plane = Plane();
     pcm = plane.calculateGlobalPCM(posA, ar)
-    MI = [0; 0; 0]
-    aa = 0
+    MI = plane.momentOfInertia
+    aa = plane.calculateAngularAcceleration(MI, ar, va, Forces)
 end
