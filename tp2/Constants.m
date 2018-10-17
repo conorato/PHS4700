a09edd9245dae737d82d7876a067b18835b48097
Claudia Onorato <clonoa@labos.polymtl.ca>
2018-10-17 14:38:02 -0400

    Added drag and gravitation forces

---
+++ tp2/Constants.m
@@ -0,0 +1,10 @@
+classdef Constants
+    properties(Constant)
+        BALL_MASS       = 0.45   % [kg]
+        BALL_RADIUS     = 0.11   % [m]
+        
+        GRAVITY         = 9.8    % [m/s^2]
+        AIR_DENSITY     = 1.2754 % [kg/m^2]
+        AIR_VISCOSITY   = 1.8e-5 % [kg/(m*s)]
+    end
+end
