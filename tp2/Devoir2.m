87e187bb5a71dd78e64e4d394e61e8d425f686b6
William Harvey <william.harvey@polymtl.ca>
2018-10-23 01:49:39 -0400

    Display goals

--- tp2/Devoir2.m
+++ tp2/Devoir2.m
@@ -1,5 +1,5 @@
 function [ But, tf, rf, vf ] = Devoir2( ri, vi, wi )
-    Display.displayField();
+    Display.init();
     time = 0;
     [lastPosition, nextPosition, lastVelocity, nextVelocity, time, brokenConstraint] = Simulation.simulateTrajectory(ri, vi, wi, Constants.DELTA_T, time);
     if(lastPosition == nextPosition) % Then initial position is already breaking a constraint 
--- tp2/Display.m
+++ tp2/Display.m
@@ -1,17 +1,33 @@
 classdef Display
     methods(Static)
-        function displayField()
+        function init()
             clf;
-            X = [0 0  120 120];
-            Y = [0 90 90  0  ];
-            Z = [0 0  0   0  ];
-            darkGreen = [0.4 0.75 0.31];
-            fill3(X, Y, Z, darkGreen);
+            Display.displayField();
+            Display.displayGoals();
             xlabel('x(m)');
             ylabel('y(m)');
             zlabel('z(m)');
             title('Trajectoire de la balle');
             axis equal;
+            hold on;
+        end
+        
+        function displayField()
+            X = [Constants.MIN_X Constants.MIN_X Constants.MAX_X Constants.MAX_X];
+            Y = [Constants.MIN_Y Constants.MAX_Y Constants.MAX_Y Constants.MIN_Y];
+            Z = [0 0 0 0];
+            darkGreen = [0.4 0.75 0.31];
+            fill3(X, Y, Z, darkGreen);
+            hold on;
+        end
+        
+        function displayGoals()
+            Display.drawLine([Constants.POST_BOTTOM_LEFT(1) Constants.POST_BOTTOM_LEFT(2) 0], [Constants.POST_BOTTOM_LEFT(1) Constants.POST_BOTTOM_LEFT(2) Constants.GOAL_HEIGHT]);
+            Display.drawLine([Constants.POST_BOTTOM_LEFT(1) Constants.POST_BOTTOM_LEFT(2) Constants.GOAL_HEIGHT], [Constants.POST_TOP_LEFT(1) Constants.POST_TOP_LEFT(2) Constants.GOAL_HEIGHT]);
+            Display.drawLine([Constants.POST_TOP_LEFT(1) Constants.POST_TOP_LEFT(2) Constants.GOAL_HEIGHT], [Constants.POST_TOP_LEFT(1) Constants.POST_TOP_LEFT(2) 0]);
+            Display.drawLine([Constants.POST_BOTTOM_RIGHT(1) Constants.POST_BOTTOM_RIGHT(2) 0], [Constants.POST_BOTTOM_RIGHT(1) Constants.POST_BOTTOM_RIGHT(2) Constants.GOAL_HEIGHT]);
+            Display.drawLine([Constants.POST_BOTTOM_RIGHT(1) Constants.POST_BOTTOM_RIGHT(2) Constants.GOAL_HEIGHT], [Constants.POST_TOP_RIGHT(1) Constants.POST_TOP_RIGHT(2) Constants.GOAL_HEIGHT]);
+            Display.drawLine([Constants.POST_TOP_RIGHT(1) Constants.POST_TOP_RIGHT(2) Constants.GOAL_HEIGHT], [Constants.POST_TOP_RIGHT(1) Constants.POST_TOP_RIGHT(2) 0]);
             hold on;
         end
 
