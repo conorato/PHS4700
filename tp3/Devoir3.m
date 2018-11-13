a083165c7e54d335010a706060ac5ac8a02e8051
conorato <c.onorato@hotmail.com>
2018-11-13 17:01:04 -0500

    added ground condition + zip

--- tp3/Devoir3.m
+++ tp3/Devoir3.m
@@ -1,32 +1,33 @@
 function [Coup, tf, vbaf, vbof, wbof, rbaf, rbof] = Devoir3(vbal, wboi, tl)
     [finalQCan, finalQBall, brokenConstraint, tf] = rungeKutta(vbal, wboi, tl);
     [vap,vbp,rap,rbp] = Physics.calculateContactPointSpeed(brokenConstraint, finalQCan, finalQBall);
-    contactPoint = CollisionDetector.getContactPoint(brokenConstraint, finalQBall(4:6), finalQCan(4:6), finalQCan(10:13))
-    n = Physics.calculateN(finalQBall(4:6), contactPoint)
     
-    %matrice d'inertie
-    Iball = [2/3 * Constants.BALL_MASS*Constants.BALL_RADIUS^2, 0, 0;
-             0, 2/3 * Constants.BALL_MASS*Constants.BALL_RADIUS^2, 0;
-             0, 0, 2/3 * Constants.BALL_MASS*Constants.BALL_RADIUS^2];
-             
-    Ican = [0.5* Constants.CAN_MASS*Constants.CAN_RADIUS^2 + 1/12 * Constants.CAN_MASS*Constants.CAN_HEIGHT^2,0,0;
-            0,0.5* Constants.CAN_MASS*Constants.CAN_RADIUS^2 + 1/12 * Constants.CAN_MASS*Constants.CAN_HEIGHT^2, 0;
-            0, 0,  Constants.CAN_MASS*Constants.CAN_RADIUS^2];
-
-    %constante pour calculer j:        
-    Gball = dot(n, cross((inv(Iball)*cross(rap, n)), rap));
-    GCan = dot(n, cross((inv(Ican)*cross(rbp, n)), rbp));
-    alpha = 1/(1/Constants.BALL_MASS + 1/Constants.CAN_MASS + Gball + GCan);
-    vr = dot(n, (vap-vbp));
-
-    %calcul de j:
-    j = -alpha *(1+ Constants.RESTITUTION_COEFFICIENT)*vr;
-    
-    %calcul de wfinal:
-    wbof = wboi + j*inv(Ican)*cross(rbp,n);
-    
-    %calcul des vitesses finales:
     if(brokenConstraint ~=Constraints.Ground)
+        contactPoint = CollisionDetector.getContactPoint(brokenConstraint, finalQBall(4:6), finalQCan(4:6), finalQCan(10:13))
+        n = Physics.calculateN(finalQBall(4:6), contactPoint)
+        
+        %matrice d'inertie
+        Iball = [2/3 * Constants.BALL_MASS*Constants.BALL_RADIUS^2, 0, 0;
+                 0, 2/3 * Constants.BALL_MASS*Constants.BALL_RADIUS^2, 0;
+                 0, 0, 2/3 * Constants.BALL_MASS*Constants.BALL_RADIUS^2];
+
+        Ican = [0.5* Constants.CAN_MASS*Constants.CAN_RADIUS^2 + 1/12 * Constants.CAN_MASS*Constants.CAN_HEIGHT^2,0,0;
+                0,0.5* Constants.CAN_MASS*Constants.CAN_RADIUS^2 + 1/12 * Constants.CAN_MASS*Constants.CAN_HEIGHT^2, 0;
+                0, 0,  Constants.CAN_MASS*Constants.CAN_RADIUS^2];
+
+        %constante pour calculer j:        
+        Gball = dot(n, cross((inv(Iball)*cross(rap, n)), rap));
+        GCan = dot(n, cross((inv(Ican)*cross(rbp, n)), rbp));
+        alpha = 1/(1/Constants.BALL_MASS + 1/Constants.CAN_MASS + Gball + GCan);
+        vr = dot(n, (vap-vbp));
+
+        %calcul de j:
+        j = -alpha *(1+ Constants.RESTITUTION_COEFFICIENT)*vr;
+
+        %calcul de wfinal:
+        wbof = wboi + j*inv(Ican)*cross(rbp,n);
+    
+        %calcul des vitesses finales:
         vapf = vap + j*(n/Constants.BALL_MASS + cross(inv(Iball)*cross(rap,n),rap));
         vbpf = vbp - j*(n/Constants.BALL_MASS + cross(inv(Ican)*cross(rbp,n),rbp));
     else
--- tp3/Physics.m
+++ tp3/Physics.m
@@ -3,11 +3,12 @@ classdef Physics
         function [vap,vbp, rap, rbp] = calculateContactPointSpeed(brokenConstraint, finalQCan, finalQBall)
             ballPos = finalQBall(4:6);
             canPos = finalQCan(4:6);
+            vap = [0;0;0]; vbp = [0;0;0]; rap = [0;0;0]; rbp = [0;0;0];
             if (brokenConstraint ~= Constraints.Ground)
                 vap = finalQBall(1:3);
                 rap = finalQBall(4:6) - CollisionDetector.getContactPoint(brokenConstraint, ballPos, canPos, finalQCan(10:13));
                 rbp = finalQCan(4:6) - CollisionDetector.getContactPoint(brokenConstraint, ballPos, canPos, finalQCan(10:13));
-                vbp = finalQCan(1:3) + cross(finalQCan(7:9), rbp);
+                vbp = finalQCan(1:3) + cross(finalQCan(7:9), rbp);                
             end
         end
 
