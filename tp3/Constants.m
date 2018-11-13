8e60f207ebbfa83b43cc09d7e19b6d804c80551d
William Harvey <william.harvey@polymtl.ca>
2018-11-12 22:44:34 -0500

    finished error control

--- tp3/Constants.m
+++ tp3/Constants.m
@@ -18,7 +18,7 @@ classdef Constants
         RESTITUTION_COEFFICIENT = 0.5;
         
         IDENTITY_QUATERNION     = [1; 0; 0; 0]; 
-        INITIAL_STEP_DISTANCE   = 0.02;                     %[m]
+        INITIAL_STEP_DISTANCE   = 0.1;                     %[m]
         EPSILON                 = [0.001, 0.001, 0.001];    %[m]
     end
 end
--- tp3/rungeKutta.m
+++ tp3/rungeKutta.m
@@ -3,15 +3,17 @@ function [finalQCan, finalQBall] = rungeKutta(vbal, wboi, tl)
     
     qCan    = [Constants.CAN_INITIAL_VELOCITY; Constants.CAN_INITIAL_POSITION; wboi; Constants.IDENTITY_QUATERNION];
     qBall   = [vbal; Constants.BALL_INITIAL_POSITION; Constants.BALL_INITIAL_ANG_VELOCITY; Constants.IDENTITY_QUATERNION];
-    t = 0; 
-    if(norm(vbal))
-        deltaT = Constants.INITIAL_STEP_DISTANCE / norm(vbal)
-    else
-        deltaT = 0.001;
+    t = 0; stepDistance = Constants.INITIAL_STEP_DISTANCE;
+    deltaT = getDeltaT(stepDistance, vbal);
+    conv = 0;
+    
+    while(~conv)
+        [qCan, qBall, t, conv] = simulate(qBall, qCan, t, tl, deltaT);
+        stepDistance = stepDistance / 10;
+        deltaT = getDeltaT(stepDistance, norm(vbal));
     end
-    [qCan, qBall, t] = simulate(qBall, qCan, t, tl, deltaT);
-    [finalQCan] = SEDRK4t0ER(qCan, t, t + deltaT, Constants.EPSILON, @gCan)
-    [finalQBall] = SEDRK4t0ER(qBall, t, t + deltaT, Constants.EPSILON, @gBall)
+    finalQCan = qCan
+    finalQBall = qBall
 end
 
 function vector = gCan(q, t)
@@ -32,7 +34,15 @@ function quaternion = getQuaternionRotation(q)
     quaternion = transpose(0.5 * QProduit(rotationQuaternion, angularVelocity));
 end
 
-function [qCan, qBall, t] = simulate(qBall, qCan, t, tl, deltaT)
+function deltaT = getDeltaT(distance, velocity)
+    if(norm(velocity))
+        deltaT = distance / norm(velocity);
+    else
+        deltaT = distance;
+    end
+end
+
+function [qCan, qBall, t, conv] = simulate(qBall, qCan, t, tl, deltaT)
     qCanPrecedent = qCan; qBallPrecedent = qBall;
     while(true)
         brokenConstraint = CollisionDetector.getBrokenConstraint(qBall(4:6), qCan(4:6), qCan(10:13));
@@ -51,7 +61,10 @@ function [qCan, qBall, t] = simulate(qBall, qCan, t, tl, deltaT)
         end
         t = deltaT + t;
     end
+    convCan = ErrSol(qCan, qCanPrecedent, Constants.EPSILON);
+    convBall = ErrSol(qBall, qBallPrecedent, Constants.EPSILON);
     qCan = qCanPrecedent;
     qBall = qBallPrecedent;
+    conv = convCan && convBall;
     t = t - deltaT;
 end
